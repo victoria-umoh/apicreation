572c243d95c91b6cb8a08ba48e317b264dc0198c
Olanrewaju Ibidokun <rico4pepe@gmail>
2024-01-30 14:59:36 +0100

    Added the getlanguage and setLanguage in one controller so the insertPreferredlamguage is not used any more

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Models\Languages;
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LanguageController extends Controller
 {
@@ -20,15 +21,40 @@ class LanguageController extends Controller
 
             foreach ($data as $country) {
                 if (isset($country['languages'])) {
-                    $languages = array_merge($languages, array_keys($country['languages']));
+                    foreach ($country['languages'] as $languageCode => $languageName) {
+                        $languages[$languageCode] = $languageName;
+                    }
                 }
             }
 
-            $uniqueLanguages = array_unique($languages);
-                return response()->json(['languages' => array_values($uniqueLanguages)], 200);
+            return response()->json(['languages' => $languages], 200);
 
         } catch (\Exception $e) {
             return response()->json(['error' => 'Unable to fetch languages.'], 500);
+        }
+    }
+
+    public function setLanguage(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'language' => 'required|exists:languages,language_code',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => 'Invalid input.'], 422);
+            }
+
+            $language = $request->input('language');
+
+            // Assuming 'languages' is the model representing your languages table
+            $lang = new Languages();
+            $lang->language_code = $language;
+            $lang->save();
+
+            return response()->json(['message' => 'Language saved successfully.', 'data' => $lang]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Unable to save language.'], 500);
         }
     }
 }
